dffce924885a75a3d0f12cd3a2da73bea5b2e7cf
Tulyakai <tulyawatt@gmail.com>
2022-05-25 01:14:45 +0700

    issue: fix error handling

--- routes/product_bp.py
+++ routes/product_bp.py
@@ -5,9 +5,9 @@ from controllers.productController import create, getById, updateById , deleteBy
 product_bp = Blueprint('symptom_bp', __name__, url_prefix='/products')
 
 product_bp.route('/create', methods=['POST'])(create)
-product_bp.route('/<int:product_id>', methods=['GET'])(getById)
-product_bp.route('/<int:product_id>/edit', methods=['POST'])(updateById)
-product_bp.route('/<int:product_id>/delete', methods=['DELETE'])(deleteById)
+product_bp.route('/<string:product_id>', methods=['GET'])(getById)
+product_bp.route('/<string:product_id>/edit', methods=['POST'])(updateById)
+product_bp.route('/<string:product_id>/delete', methods=['DELETE'])(deleteById)
 product_bp.route('/query-symptom', methods=['POST'])(querySymptom)
 
 

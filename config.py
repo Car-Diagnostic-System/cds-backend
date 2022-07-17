64483273d577577b0ad927d3bf81ee386b02853d
Tulyakai <tulyawatt@gmail.com>
2022-07-17 12:31:28 +0700

    chore: change db uri port

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'mysql://root:password@127.0.0.1:3366/cds'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:password@127.0.0.1:3306/cds'
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_SORT_KEYS = False

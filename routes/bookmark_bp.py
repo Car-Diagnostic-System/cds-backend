8576e99293a041b9f78ccf6a07acadfc70344e98
Tulyakai <tulyawatt@gmail.com>
2022-07-24 22:29:46 +0700

    fix: change getBookmarkByUserId http method

--- routes/bookmark_bp.py
+++ routes/bookmark_bp.py
@@ -3,6 +3,6 @@ from controllers.bookmarkController import BookmarkController
 
 class BookmarkBlueprint:
     bookmark_bp = Blueprint('bookmark_bp', __name__, url_prefix='/bookmarks')
-    bookmark_bp.route('/', methods=['GET'])(BookmarkController.getBookmarkByUserId)
+    bookmark_bp.route('/', methods=['POST'])(BookmarkController.getBookmarkByUserId)
     bookmark_bp.route('/add', methods=['POST'])(BookmarkController.addBookmark)
     bookmark_bp.route('/remove', methods=['POST'])(BookmarkController.removeBookmark)

c7954924a18ee1794651760f7e1f84b76cfd96b7
Tulyakai <tulyawatt@gmail.com>
2022-07-26 16:44:37 +0700

    feat: update init value

--- models/__init__.py
+++ models/__init__.py
@@ -54,10 +54,10 @@ def create_role(*args, **kwargs):
 @event.listens_for(User.__table__, 'after_create')
 def create_user(*args, **kwargs):
     print('Inserting user table')
-    db.session.add(User(imageProfile='image.png', firstname='ตุลยวัต', lastname='ทับคง', email='tulyawatt@gmail.com', password=bcrypt.hashpw('password'.encode('utf-8'), bcrypt.gensalt(10)), role=2, car=None))
-    db.session.add(User(imageProfile='image.png', firstname='กฤตพร', lastname='แก้วปิยรัตน์', email='krittaporn.tong@gmail.com', password=bcrypt.hashpw('1234'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=1016))
-    db.session.add(User(imageProfile='image.png', firstname='พรมงคง', lastname='พุทธิแจ่ม', email='fax@gmail.com', password=bcrypt.hashpw('faxpassword'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=322))
-    db.session.add(User(imageProfile='image.png', firstname='ภาสกร', lastname='เปียงใจ', email='kong@gmail.com', password=bcrypt.hashpw('kongpassword'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=921))
+    db.session.add(User(imageProfile='image.png', firstname='ตุลยวัต', lastname='ทับคง', email='tulyawatt@gmail.com', password=bcrypt.hashpw('kaiPassword'.encode('utf-8'), bcrypt.gensalt(10)), role=2, car=None))
+    db.session.add(User(imageProfile='image.png', firstname='กฤตพร', lastname='แก้วปิยรัตน์', email='krittaporn.tong@gmail.com', password=bcrypt.hashpw('tongPassword'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=1016))
+    db.session.add(User(imageProfile='image.png', firstname='พรมงคง', lastname='พุทธิแจ่ม', email='fax@gmail.com', password=bcrypt.hashpw('faxPassword'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=322))
+    db.session.add(User(imageProfile='image.png', firstname='ภาสกร', lastname='เปียงใจ', email='kong@gmail.com', password=bcrypt.hashpw('kongPassword'.encode('utf-8'), bcrypt.gensalt(10)), role=1, car=921))
     db.session.commit()
 
 @event.listens_for(Bookmark.__table__, 'after_create')
